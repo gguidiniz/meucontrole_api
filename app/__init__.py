8c7bbb6604a05a0512a2c160fe1f023890accb27
Guilherme Diniz <guilhermeortizdiniz@gmail.com>
2025-11-30 16:34:56 -0300

    teste

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from .config import Config
+from test import teste
 
 db = SQLAlchemy()
 migrate = Migrate()
